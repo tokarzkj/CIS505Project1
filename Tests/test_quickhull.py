81c43f386eca09e44720d322675caa69373933f5
= <=>
2023-11-04 16:22:57 -0400

    Add the extra quickhull tests for colinear points

--- Tests/test_quickhull.py
+++ Tests/test_quickhull.py
@@ -1,3 +1,4 @@
+from copy import copy
 import pytest
 from Algorithms.quickhull import QuickHull
 from point import Point
@@ -22,6 +23,25 @@ def test_all_points_are_hull_points():
 
 def test_some_points_are_hull_points():
     points = [Point(1, 2), Point(3, 3), Point(5, 6), Point(6, 1)]
+    hull = QuickHull(points)
+
+    answer = [points[0], points[2], points[3]]
+    assert all(p in hull for p in answer)
+
+# Confirm that when there are colinear points and we encounter the furthest point first we keep that point for the convex hull.
+def test_colinear_points_with_further_point_first():
+    points = [Point(1,2), Point(3,6), Point(2, 4), Point(4,1)]
+
+    # QuickHull starts by sorting the points and as a result it changes the variable here. I use copy to avoid changing the order of points
+    # in this function, so I can use my hack for making sure the QuickHull results are correct.
+    hull = QuickHull(copy(points))
+
+    answer = [points[0], points[1], points[3]]
+    assert all(p in hull for p in answer)
+
+# Confirm that when there are colinear points and we encounter the closer point first we switch to the further point for the convex hull.
+def test_colinear_points_with_closer_point_first():
+    points = [Point(1,2), Point(2, 4), Point(3,6), Point(4,1)]
     hull = QuickHull(points)
 
     answer = [points[0], points[2], points[3]]
